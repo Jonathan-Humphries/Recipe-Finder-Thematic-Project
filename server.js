d86baeffa410ec2e08c649b31ad30cb167bb7388
HugeF0rhead <jonathanhumphries5@outlook.com>
2026-02-28 16:58:54 +0000

    removed some errors

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
 const express = require('express')
 const pool = require('./db')
+const path = require('path')
 const port = 3000
 
 const app = express()
 app.use(express.json())
 
-app.get('/', async (req, res) =>{
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/ingredients', async (req, res) =>{
     try{
         const data = await pool.query('SELECT * FROM ingredients')
         res.status(200).send({ingredients: data.rows})
 
     }   catch (err){
-            console.log(error)
+            console.log(err)
             res.sendStatus(500)
     } 
-    res.sendStatus(200)
 })
 
-app.post('/', async (req, res) => {
+app.post('/ingredients', async (req, res) => {
     const {name, calories, protein, carbohydrates, sugar, fat, salt} = req.body
     try{
-        await pool.query('INSERT INTO ingredients (name, calories, protein, carbohydrates, sugar, fat, salt) VALUES ($1, $2)', [name, calories, protein, carbohydrates, sugar, fat, salt])
+        await pool.query('INSERT INTO ingredients (name, calories, protein, carbohydrates, sugar, fat, salt) VALUES ($1, $2, $3, $4, $5, $6, $7)', [name, calories, protein, carbohydrates, sugar, fat, salt])
         res.status(200).send({message: "Successfully added ingredient"})
 
     }   catch (err){
-            console.log(error)
+            console.log(err)
             res.sendStatus(500)
     } 
 })
 
 app.get('/setup', async (req, res) => {
     try{
-        await pool.query('CREATE TABLE ingredients( id SERIAL PRIMARY KEY, name VARCHAR(100), calories int, protein int, carbohydrates int, sugar int, fat int, salt int, )')
+        await pool.query('CREATE TABLE IF NOT EXISTS ingredients( id SERIAL PRIMARY KEY, name VARCHAR(100), calories int, protein int, carbohydrates int, sugar int, fat int, salt int )')
+        res.status(200).send("Table created")
     } catch (err){
         console.log(err)
         res.sendStatus(500)
