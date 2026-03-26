42cf5fd9b3c8557d00b717cdf61504b5512da86a
Jonathan Michael Humphries <24804449@stu.mmu.ac.uk>
2026-03-26 12:18:24 +0000

    Created .sql file and created 2 main tables for 'ingredients' and 'recipes' with a weak entity inbetween

---
+++ public/sample.sql
@@ -0,0 +1,34 @@
+CREATE TABLE ingredient(
+	id SERIAL PRIMARY KEY NOT NULL,
+
+	name TEXT NOT NULL,
+
+	calories INT NOT NULL, --per 100g
+
+	protein INT NOT NULL,
+
+	carbohydrates INT NOT NULL,
+
+	sugar INT NOT NULL,
+
+	fat INT NOT NULL,
+
+	salt INT NOT NULL,
+)
+
+CREATE TABLE recipe_ingredient (
+    recipe_id INT REFERENCES recipe(id),
+    ingredient_id INT REFERENCES ingredient(id),
+    quantity NUMERIC,
+    unit TEXT,
+    PRIMARY KEY (recipe_id, ingredient_id)
+);
+
+
+CREATE TABLE recipe(
+	id SERIAL PRIMARY KEY NOT NULL,
+
+	name TEXT NOT NULL,
+
+	instructions TEXT NOT NULL,
+)
